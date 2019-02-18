aeb432bba8934167a6aaca35bbb60621ea3eceb0
Tiago Pierezan Camargo <camargo@discworld.com.br>
2019-02-18 16:54:51 -0300

    ES: initial package for retropie-es

---
+++ package/batocera/emulationstation/retropie-emulationstation/retropie-emulationstation.mk
@@ -0,0 +1,43 @@
+################################################################################
+#
+# Batocera Emulation Station
+#
+################################################################################
+
+RETROPIE_EMULATIONSTATION_VERSION = 8878cb0b4c2865ff28abaacec12f31507c18c1d5
+RETROPIE_EMULATIONSTATION_SITE = https://github.com/RetroPie/EmulationStation
+RETROPIE_EMULATIONSTATION_SITE_METHOD = git
+RETROPIE_EMULATIONSTATION_GIT_SUBMODULES = YES
+RETROPIE_EMULATIONSTATION_LICENSE = MIT
+RETROPIE_EMULATIONSTATION_DEPENDENCIES = sdl2 sdl2_mixer boost libfreeimage freetype eigen alsa-lib \
+	libcurl openssl vlc rapidjson
+
+ifeq ($(BR2_PACKAGE_KODI),y)
+	RETROPIE_EMULATIONSTATION_CONF_OPTS += -DDISABLE_KODI=0
+else
+	RETROPIE_EMULATIONSTATION_CONF_OPTS += -DDISABLE_KODI=1
+endif
+#
+# ifeq ($(BR2_PACKAGE_XORG7),y)
+# 	BATOCERA_EMULATIONSTATION_CONF_OPTS += -DENABLE_FILEMANAGER=1
+# else
+# 	BATOCERA_EMULATIONSTATION_CONF_OPTS += -DENABLE_FILEMANAGER=0
+# endif
+
+ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
+RETROPIE_EMULATIONSTATION_DEPENDENCIES += libgl
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_LIBGLES),y)
+RETROPIE_EMULATIONSTATION_DEPENDENCIES += libgles
+endif
+
+define RETROPIE_EMULATIONSTATION_RPI_FIXUP
+	$(SED) 's|/opt/vc/include|$(STAGING_DIR)/usr/include|g' $(@D)/CMakeLists.txt
+	$(SED) 's|/opt/vc/lib|$(STAGING_DIR)/usr/lib|g' $(@D)/CMakeLists.txt
+	$(SED) 's|/usr/lib|$(STAGING_DIR)/usr/lib|g' $(@D)/CMakeLists.txt
+endef
+
+RETROPIE_EMULATIONSTATION_PRE_CONFIGURE_HOOKS += RETROPIE_EMULATIONSTATION_RPI_FIXUP
+
+$(eval $(cmake-package))
