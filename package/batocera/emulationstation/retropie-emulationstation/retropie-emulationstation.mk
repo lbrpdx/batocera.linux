89370cb4b6b90bbdbc144c810efccaf3b8785f5e
Tiago Pierezan Camargo <camargo@discworld.com.br>
2019-02-19 21:51:45 -0300

    R-ES: switch to stable branch

--- package/batocera/emulationstation/retropie-emulationstation/retropie-emulationstation.mk
+++ package/batocera/emulationstation/retropie-emulationstation/retropie-emulationstation.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-RETROPIE_EMULATIONSTATION_VERSION = 8878cb0b4c2865ff28abaacec12f31507c18c1d5
+# Version: 2.8.1
+RETROPIE_EMULATIONSTATION_VERSION = stable
 RETROPIE_EMULATIONSTATION_SITE = https://github.com/RetroPie/EmulationStation
 RETROPIE_EMULATIONSTATION_SITE_METHOD = git
 RETROPIE_EMULATIONSTATION_GIT_SUBMODULES = YES
-RETROPIE_EMULATIONSTATION_LICENSE = MIT
+RETROPIE_EMULATIONSTATION_LICENSE = MIT, Apache-2.0
 RETROPIE_EMULATIONSTATION_DEPENDENCIES = sdl2 sdl2_mixer boost libfreeimage freetype eigen alsa-lib \
 	libcurl openssl vlc rapidjson
 
